fb77120c3149b59a3b435630faba79519f2de2b2
jbijlsma <jeroen_work@yahoo.com>
2015-05-03 21:00:36 +0200

    remove duplication

--- sync.sh
+++ sync.sh
@@ -1,9 +1,12 @@
+DOCKER_IP=$(boot2docker ip)
+PWD=$(pwd)
+
+# rsync the working directory to b2d host
 eval `ssh-agent -s`
 ssh-add ~/.ssh/id_boot2docker
+rsync -av --chmod=ugo=rwX --exclude-from 'exclude.txt' ./ docker@$DOCKER_IP:$(pwd)
 
-
-rsync -av --chmod=ugo=rwX --exclude-from 'exclude.txt' ./ docker@$(boot2docker ip 2>/dev/null):$(pwd)
-
-boot2docker ssh "sudo kill \$(docker top \$(docker ps | grep nancy_instance | awk '{print \$1}') | grep mono | awk '{print \$1}');
-		docker exec \$(docker ps | grep nancy_instance | awk '{print \$1}') xbuild;
-		docker exec -d \$(docker ps | grep nancy_instance | awk '{print \$1}') mono src/bin/Nancy.Docker.exe"
+boot2docker ssh "NANCY_CONTAINER_ID=\$(docker ps | grep nancy_instance | awk '{print \$1}');
+		sudo kill \$(docker top \$NANCY_CONTAINER_ID | grep mono | awk '{print \$1}');
+		docker exec \$NANCY_CONTAINER_ID xbuild;
+		docker exec -d \$NANCY_CONTAINER_ID mono src/bin/Nancy.Docker.exe"
